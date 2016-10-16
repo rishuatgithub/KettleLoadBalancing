bc75f3b443a0196f0c8bd1b397d137387e3a9826
Rishu Shrivastava <rishu.shrivastava@gmail.com>
2016-10-17 00:02:42 +0100

    test jobs entry

--- Scripts - DDL/PDILoadBalancing.sql
+++ Scripts - DDL/PDILoadBalancing.sql
@@ -16,7 +16,7 @@ slave_last_modified timestamp
 /* Manual Slave Server Data entry */
 insert into public.slave_server_list values(1001,'localhost',8084,'cluster','cluster','N',2,0,0);
 insert into public.slave_server_list values(1002,'localhost',8085,'cluster','cluster','N',1,0,0);
-insert into public.slave_server_list values(1003,'localhost',8086,'cluster','cluster','N',3,0,0);
+insert into public.slave_server_list values(1003,'localhost',8086,'cluster','cluster','N',1,0,0);
 
 
 select * from public.slave_server_list;
@@ -48,16 +48,86 @@ drop table public.queue_jobslave_list;
 drop table public.job_order_list;
 
 
-insert into public.job_order_list values('100','test1.ktr','F:\Pentaho Load Balancing\TestJobs\',1,'0','NOTSTARTED',now());
-insert into public.job_order_list values('101','test2.ktr','F:\Pentaho Load Balancing\TestJobs\',2,'1','NOTSTARTED',now());
-insert into public.job_order_list values('102','test3.ktr','F:\Pentaho Load Balancing\TestJobs\',2,'1','NOTSTARTED',now());
-insert into public.job_order_list values('103','test4.ktr','F:\Pentaho Load Balancing\TestJobs\',3,'2','NOTSTARTED',now());
-insert into public.job_order_list values('104','test5.ktr','F:\Pentaho Load Balancing\TestJobs\',4,'3','NOTSTARTED',now());
+insert into public.job_order_list values('1','test1.ktr','F:\Pentaho Load Balancing\TestJobs\',1,'1','NOTSTARTED',now());
+insert into public.job_order_list values('2','test2.ktr','F:\Pentaho Load Balancing\TestJobs\',2,'2','NOTSTARTED',now());
+insert into public.job_order_list values('3','test3.ktr','F:\Pentaho Load Balancing\TestJobs\',3,'2','NOTSTARTED',now());
+insert into public.job_order_list values('4','test4.ktr','F:\Pentaho Load Balancing\TestJobs\',4,'4','NOTSTARTED',now());
+insert into public.job_order_list values('5','test5.ktr','F:\Pentaho Load Balancing\TestJobs\',5,'5','NOTSTARTED',now());
 
 select * from public.job_order_list;
 select * from public.queue_jobslave_list;
 
 truncate public.queue_jobslave_list;
 truncate public.job_order_list;
+
+
+
+-- Transformation log table
+--
+
+CREATE TABLE "public".slave_execution_logging
+(
+  ID_BATCH INTEGER
+, CHANNEL_ID VARCHAR(255)
+, TRANSNAME VARCHAR(255)
+, STATUS VARCHAR(15)
+, LINES_READ BIGINT
+, LINES_WRITTEN BIGINT
+, LINES_UPDATED BIGINT
+, LINES_INPUT BIGINT
+, LINES_OUTPUT BIGINT
+, LINES_REJECTED BIGINT
+, ERRORS BIGINT
+, STARTDATE TIMESTAMP
+, ENDDATE TIMESTAMP
+, LOGDATE TIMESTAMP
+, DEPDATE TIMESTAMP
+, REPLAYDATE TIMESTAMP
+, LOG_FIELD TEXT
+, EXECUTING_SERVER VARCHAR(255)
+, EXECUTING_USER VARCHAR(255)
+, CLIENT VARCHAR(255)
+)
+;
+CREATE INDEX IDX_slave_execution_logging_1 ON "public".slave_execution_logging(ID_BATCH)
+;
+CREATE INDEX IDX_slave_execution_logging_2 ON "public".slave_execution_logging(ERRORS, STATUS, TRANSNAME)
+;
+
+
+select transname, max(logdate) from "public".slave_execution_logging
+group by transname
+truncate public.slave_execution_logging;
+
+
+
+
+
+-- SLAVE META DATA :         
+select * from public.slave_server_list order by 1;
+-- JOB META DATA   :         
+select * from public.job_order_list order by 1;
+-- QUEUE META DATA :         
+select * from public.queue_jobslave_list order by 1;
+-- LOG META DATA   :         
+select * from public.slave_execution_logging order by 1;
+
+
+
+
+
+
+
+truncate public.job_order_list;
+truncate public.queue_jobslave_list;
+truncate public.slave_execution_logging;
+
+
+
+
+
+
+
+
 
 
